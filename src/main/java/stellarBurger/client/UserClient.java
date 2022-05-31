9a9daf4a0abf30042f46f821b75be4478c8bf7da
Elizaveta Perevedentseva <lisaperev@yandex.ru>
2022-05-31 22:05:22 +0300

    first commit to develop

--- src/main/java/stellarBurger/client/UserClient.java
+++ src/main/java/stellarBurger/client/UserClient.java
@@ -2,6 +2,10 @@ package stellarBurger.client;
 
 public class UserClient {
 
+        private final String JSON = "application/json";
+        private final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+        private final String REGISTER_USER_PATH = "/api/auth/register";
+        private final String AUTH_USER_PATH = "/api/auth/login";
 
 
         }
